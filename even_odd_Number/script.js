6edcceddc22ce0f7684ccde50c4426e6997b92cc
ibrahimohid <ibrahimohid77@gmail.com>
2023-12-31 06:53:28 +0600

    odd and even number project

---
+++ even_odd_Number/script.js
@@ -0,0 +1,8 @@
+const  userInput = parseInt(prompt('Enter a Number to see it is a Even Or Odd Number:'));
+
+if (userInput % 2 == 0) {
+    alert('The Number is a Even Number');
+}
+else{
+    alert('THE NUMBER IS ODD NUMBER');
+}
