eddb1f51015b21fed067e467514fce6e727b201f
ibrahimohid <ibrahimohid77@gmail.com>
2024-01-07 21:46:22 +0600

    todo app added

---
+++ todo_app/script.js
@@ -0,0 +1,18 @@
+// find the element
+const InputText = document.querySelector('#input-text');
+const AddTodo = document.querySelector('#add-todo');
+const Lists = document.querySelector('#lists');
+const TodoRemove = document.querySelector('#todo-remove');
+
+
+const newTodo = (event) => {
+    event.preventDefault();
+    const TodoValue = InputText.value;
+    const TodoElement = document.createElement('li');
+    TodoElement.innerHTML = `<span>${TodoValue}</span> <span><button class="btn-primary px-3 py-1 rounded-md border" id="todo-remove">Remove</button></span>`;
+    Lists.appendChild(TodoElement);
+    InputText.value = '';
+}
+
+//addEventListener
+AddTodo.addEventListener('click', newTodo);
