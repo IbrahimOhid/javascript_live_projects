c9bbbc9f08a97f19245cf1882400105fd8581aeb
ibrahimohid <ibrahimohid77@gmail.com>
2024-01-15 22:08:35 +0600

    word count js updated

--- word-and-character-count/script.js
+++ word-and-character-count/script.js
@@ -1,13 +1,29 @@
-// select element
+// select all needed element
 const Word = document.querySelector('#word');
 const Character = document.querySelector('#character');
 const Textarea = document.querySelector('#textarea');
 const ClearBtn = document.querySelector('#clear-btn');
 
-// EventListener
+// EventListener Textarea
 Textarea.addEventListener('input', () => {
-    const textLength = Textarea.value.length;
+    const textLength = Textarea.value.replace(/\s/g, '').length;
+
+    // character count
+    Character.innerHTML = textLength;
+
+    //word count
+    let getWord = Textarea.value;
+    getWord = getWord.trim();
+
+    const outputWord = getWord.split(' ');
+    Word.innerHTML = outputWord.length;
+
     
-    //total word
-    Word.innerHTML = textLength;
 })
+// EventListener ClearBtn
+ClearBtn.addEventListener('click', ()=> {
+    Textarea.value = ''
+    Character.innerHTML = 0;
+    Word.innerHTML = 0;
+})
+
