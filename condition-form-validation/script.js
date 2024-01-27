12f3bb60e4c6705ef1b6c670f14ba513c68fc92e
ibrahimohid <ibrahimohid77@gmail.com>
2024-01-27 12:03:41 +0600

    condition form validation added

---
+++ condition-form-validation/script.js
@@ -0,0 +1,31 @@
+// select all needed element
+const BanglaNumber = document.querySelector('#bangla-number');
+const EnglishNumber = document.querySelector('#english-number');
+
+
+
+// EventListener Bangla Marks number 
+BanglaNumber.addEventListener('input', (event) => {
+    event.preventDefault();
+    if (BanglaNumber.value > 100) {
+        BanglaNumber.style.background = 'blue';
+        BanglaNumber.style.color = 'white';
+    }
+    if (BanglaNumber.value < 0) {
+        BanglaNumber.style.background = 'black';
+        BanglaNumber.style.color = 'white';
+    }
+})
+
+// EventListener English Marks number 
+EnglishNumber.addEventListener('input', (event) => {
+    event.preventDefault();
+    if (EnglishNumber.value > 100) {
+        EnglishNumber.style.background = 'red';
+        EnglishNumber.style.color = 'white';
+    }
+    if (EnglishNumber.value < 0) {
+        EnglishNumber.style.background = 'green';
+        EnglishNumber.style.color = 'white';
+    }
+})
