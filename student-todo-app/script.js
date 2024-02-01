f9ca10606e62b02d1af53fc313b028dc8f20d3ac
ibrahimohid <ibrahimohid77@gmail.com>
2024-02-01 22:17:46 +0600

    student todo app

---
+++ student-todo-app/script.js
@@ -0,0 +1,7 @@
+//  select all needed element
+const Name = document.querySelector('#name');
+const Id = document.querySelector('#id');
+const Department = document.querySelector('#department');
+const Gpa = document.querySelector('#gpa');
+const AddStudent = document.querySelector('#add-student');
+
