2ae50e09d7bb94872f51463f6d35e1ca1b1dcec6
ibrahimohid <ibrahimohid77@gmail.com>
2024-02-02 20:34:50 +0600

    todo app update

--- student-todo-app/script.js
+++ student-todo-app/script.js
@@ -5,3 +5,18 @@ const Department = document.querySelector('#department');
 const Gpa = document.querySelector('#gpa');
 const AddStudent = document.querySelector('#add-student');
 
+const NewTableData = document.querySelector('#new-table-data');
+
+
+
+// EventListener
+AddStudent.addEventListener('click', (event)=> {
+    event.preventDefault();
+    const NameValue = Name.value;
+    const IdValue = Id.value;
+    const DepartmentValue = Department.value;
+    const GpaValue = Gpa.value;
+    
+    const NewTableElement = document.createElement('td');
+    NewTableElement.appendChild(NameValue);
+})
