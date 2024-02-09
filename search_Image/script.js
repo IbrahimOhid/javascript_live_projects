9e331e145e8fb7b6ff69515fd7e837bc12120e5d
ibrahimohid <ibrahimohid77@gmail.com>
2024-02-09 11:56:06 +0600

    search image

---
+++ search_Image/script.js
@@ -0,0 +1,18 @@
+// select all needed element
+const InputText = document.querySelector('#input-text');
+
+
+InputText.addEventListener('input', () => {
+    const Image = document.querySelectorAll('.image');
+    var ImgLen = Image.length;
+    for (var i = 0; i < ImgLen; i++) {
+        Image[i].style.display = 'none';
+    }
+
+    for (var j = 0; j < ImgLen; j++) {
+        var allTitle = Image[j].getAttribute('title');
+        if (InputText.value == allTitle) {
+            Image[j].style.display = 'block';
+        }
+    }
+})
