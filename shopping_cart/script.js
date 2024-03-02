8c7749a471eeae8ecbf81ebe34d34df07d6f8865
ibrahimohid <ibrahimohid77@gmail.com>
2024-03-02 21:47:39 +0600

    item amonut added

--- shopping_cart/script.js
+++ shopping_cart/script.js
@@ -1,38 +1,30 @@
 // Plus Button
-function increment(IncDec){
-    const count = document.getElementById(IncDec);
-
+function increment(Inc, prc, itm){
+    const count = document.getElementById(Inc);
+    const price = document.getElementById(prc);
+    const item = document.getElementById(itm);
     if(count.value >= 5){
         count.value = 5;
-        alert('Only Five Product Add');
+        alert('Only Five Product Added');
     }
     else{
         count.value++;
+        const ItemAmount = parseInt(price.innerHTML) + parseInt(item.innerHTML);
+        item.innerHTML = ItemAmount;
     }
 }
 
 // Minus Button
-function decrement(IncDec){
-    const count = document.getElementById(IncDec);
-    
+function decrement(Dec, prc, itm){
+    const count = document.getElementById(Dec);
+    const price = document.getElementById(prc);
+    const item = document.getElementById(itm);
     if(count.value <= 0){
         count.value = 0;
     }
     else{
         count.value--;
+        const ItemAmount = parseInt(item.innerHTML) - parseInt(price.innerHTML);
+        item.innerHTML = ItemAmount;
     }
-}
-
-//select all Needed product price 
-const Price1 = document.querySelector('#price1');
-const Price2 = document.querySelector('#price2');
-const Price3 = document.querySelector('#price3');
-
-
-//add to cart event
-function addToCart(){
-const ProductOnePrice =  parseInt(Price1.textContent)  * count.value;
-const ProductTw0Price =  parseInt(Price2.textContent)  * count.value;
-const ProductThreePrice =  parseInt(Price3.textContent)  * count.value;
-
 }
